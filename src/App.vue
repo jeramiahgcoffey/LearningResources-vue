8efc755e428cfdc248fc010df29a411ce93aa86b
jeramiahgcoffey <jerrygcoffey@gmail.com>
2022-05-22 14:40:26 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <stored-resources :resources="storedResources" />
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+
+export default {
+  components: {
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official documentation for Vue.js 3',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: "It's Google!",
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <ul>
+      <learning-resource
+        v-for="res in resources"
+        :key="res.id"
+        :title="res.title"
+        :description="res.description"
+        :link="res.link"
+      />
+    </ul>
+  </main>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  props: {
+    resources: Array,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
